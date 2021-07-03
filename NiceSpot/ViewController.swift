3a32cf2aa007fa72fb4d75df0298d11425e0dc79
Ludovic HENRY <ludovic.henry@me.com>
2021-07-02 23:33:22 -0400

    Initial Commit

---
+++ NiceSpot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NiceSpot
+//
+//  Created by Ludovic HENRY on 02/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
