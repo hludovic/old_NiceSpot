aad790e52664c0599a9721611cde8c21ea3a7448
Ludovic HENRY <badsteam@hotmail.com>
2021-11-16 15:17:01 -0400

    The subscriptionManager can enable the notifications

---
+++ NiceSpot/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  NiceSpot
+//
+//  Created by Ludovic HENRY on 22/10/2021.
+//
+
+import Foundation
---
+++ NiceSpot/Supporting files/ProjectErrors.swift
@@ -0,0 +1,8 @@
+//
+//  ProjectErrors.swift
+//  NiceSpot
+//
+//  Created by Ludovic HENRY on 21/10/2021.
+//
+
+import Foundation
